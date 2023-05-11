e6ab2a0e97047963bba38d02c5b5be239de7803c
LiuN1an <kungfupanda2016@163.com>
2023-05-12 00:41:27 +0800

    feat: 增加重试异步函数的钩子

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -14,3 +14,48 @@ export const useTimeout = (timeout = 1000): [boolean, () => void] => {
 
   return [isEnter, startTimeout];
 };
+
+/**
+ * 一直重试异步函数直到成功，直到超时
+ * 如果异步函数throw错误,则会重试，如果不报错，就会显示通过
+ */
+export const useRetry = (
+  cb: () => Promise<void>,
+  props?: { maxRetryCount?: number; immediate?: boolean; timeout?: number }
+) => {
+  const {
+    maxRetryCount = 30,
+    immediate = true,
+    timeout = 1500,
+  } = props || {};
+  const [isAccess, setAccess] = useState(false);
+  const [overLimit, setOverLimit] = useState(false);
+
+  const tryFetch = useCallback(
+    async (count: number) => {
+      if (count > 0) {
+        try {
+          await cb();
+          setAccess(true);
+        } catch (e) {
+          setTimeout(() => tryFetch(count - 1), timeout);
+        }
+      } else {
+        setOverLimit(true);
+      }
+    },
+    [timeout]
+  );
+
+  useEffect(() => {
+    if (immediate) {
+      tryFetch(maxRetryCount);
+    }
+  }, [tryFetch, maxRetryCount, immediate]);
+
+  return {
+    access: isAccess,
+    overLimit,
+    trigger: immediate ? undefined : () => tryFetch(maxRetryCount),
+  };
+};
